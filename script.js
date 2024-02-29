6e3fa5fa77ee83a6165c73822a7e4b9d3d9255f4
SalmaCano <l19201152@pachuca.tecnm.mx>
2024-02-29 12:08:17 -0600

    version inicial

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const startButton = document.getElementById('startButton');
 const speechResult = document.getElementById('speechResult');
 
 const recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+// Establecer el idioma de reconocimiento de voz 
 recognition.lang = 'es-ES';
 
 startButton.addEventListener('click', () => {
